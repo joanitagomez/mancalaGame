2e1e25850545b49b90330514a35726a9a8743848
joanitagomez@gmail.com <joanitagomez@gmail.com>
2017-05-02 20:31:51 -0700

    completed project

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -6,58 +6,46 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
+
 /**
- * @author Ada
- * Team members: Han , Joanitha  
- *  MancalaTester Runs the main program. It also acts as the controller.
+ * @author Ada Team members: Han , Joanitha MancalaTester Runs the main program.
+ *  Class also acts as the controller.
  */
 public class MancalaTest {
 
-	 /**
-	    * The main method starts the program.
-	    * @param args String [] no value
-	    */
+	/**
+	 * The main method starts the program.
+	 * @param args String [] no value
+	 */
 	public static void main(String[] args) {
 
 		JFrame frame = new JFrame();
 		frame.setSize(900, 600);
 		frame.setTitle("Mancala");
 
-		
-//		String strStones = JOptionPane.showInputDialog("How many stones do you want to play with (3 or 4)? ");
-//		 if(strStones.equals(""))
-//				System.exit(0);
-//		 
-//		int stones = Integer.parseInt(strStones);
-//		if (!strStones.equals("3") && !strStones.equals("4")){
-//			JOptionPane.showMessageDialog(null,"Invalid number. You've been assigned 3 stones.");
-//			stones = 3;
-//		}
-//		
-		
 		Object[] strStones = { "4", "3" };
 		int stones = JOptionPane.showOptionDialog(null, "Pick number of stones", "Stones", JOptionPane.DEFAULT_OPTION,
 				JOptionPane.PLAIN_MESSAGE, null, strStones, strStones[1]);
-		
-		if(stones == 1)
+
+		if (stones == 1)
 			stones = 3;
 		else
 			stones = 4;
-		
+
 		Object[] strategy = { "Alternate", "Normal" };
 		int style = JOptionPane.showOptionDialog(null, "Choose a Style", "Style", JOptionPane.DEFAULT_OPTION,
 				JOptionPane.PLAIN_MESSAGE, null, strategy, strategy[1]);
-		
+
 		StyleManager sm;
-		
-		if(style == 1)
-			 sm = new DefaultStyle ();
+
+		if (style == 1)
+			sm = new DefaultStyle();
 		else
 			sm = new AltStyle();
 
 		final Model model = new Model(stones);
 		final MancalaView view = new MancalaView(model, sm);
-		
+
 		final JButton undoButton = new JButton("Undo");
 		undoButton.setEnabled(false);
 
@@ -77,19 +65,18 @@ public class MancalaTest {
 					return;
 				if (model.isValidPit(pit)) {
 					model.updatePits(pit);
-					
+
 					if (model.getGameState() == 1 && model.getUndoA() > 0)
 						undoButton.setEnabled(true);
-					
+
 					else if (model.getGameState() == 0 && model.getUndoB() > 0)
 						undoButton.setEnabled(true);
-					
+
 					else
 						undoButton.setEnabled(false);
-				}
-				else
+				} else
 					JOptionPane.showMessageDialog(null, "Invalid Pit");
-					
+
 				if (model.isGameOver()) {
 					view.label.setHorizontalAlignment(SwingConstants.LEFT);
 					view.label.setText("Game Over. Player " + view.stringGameState(model.declareWinner()) + " wins!");
